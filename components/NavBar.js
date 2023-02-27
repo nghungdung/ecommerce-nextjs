937a3a18335b1454b0d1609580042a0fcb0e7896
thongnth <haithong1993@gmail.com>
2023-02-27 15:36:16 +0700

    nè bé dũng

--- components/NavBar.js
+++ components/NavBar.js
@@ -10,14 +10,20 @@ function NavBar() {
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
+
+      <p>
+        <i class="bi bi-basket2-fill" style={{ fontSize: 50 }}></i>
+      </p>
+      
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <Link  href="/cart" className="nav-link" >
-              <i class="fa-solid fa-cart-shopping"></i>
+
+            <Link href="/cart" className="nav-link" >
+
               Cart
             </Link>
-            
+
           </li>
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import Layout from '../components/Layout'
 
 export default function App({ Component, pageProps }) {
