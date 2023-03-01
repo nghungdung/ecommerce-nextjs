4c5fa8a0c46f81c6f0830e6da4b62c5b3d41f255
Dung Hung Nguyen <dungnov21@gmail.com>
2023-03-01 11:43:01 +0700

    new

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Layout from '../components/Layout'
+import { DataProvider } from '../store/GlobalState';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps}/>
-    </Layout>
+    <DataProvider>
+      <Layout>
+        <Component {...pageProps}/>
+      </Layout>
+    </DataProvider>
+
   )
 }
