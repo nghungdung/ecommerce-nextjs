916782be338527bcb9e908fcca37c8a51217d030
Dung Hung Nguyen <dungnov21@gmail.com>
2023-03-28 11:32:08 +0700

    new

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -17,13 +17,14 @@ const updateRole = async (req, res) => {
     try {
        const result = await auth(req, res)
        if(result.role !== 'admin' || result.root) 
-       return res.status(400).json({err: "Authentication is not valid"})
+        return res.status(400).json({err: "Authentication is not valid"})
         
        const {id} = req.query
        const {role} = req.body
     
        await Users.findOneAndUpdate({_id: id}, {role})
        res.json({msg: 'Update Success'})
+       
     } catch (err) {
         return res.status(500).json({err: err.message})
     }
