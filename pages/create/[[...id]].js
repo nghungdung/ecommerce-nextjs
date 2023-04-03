7aabe66477a15a6e0f5354657ea362e3ea82a61a
Dung Hung Nguyen <dungnov21@gmail.com>
2023-04-03 11:32:14 +0700

    new

--- pages/create/[[...id]].js
+++ pages/create/[[...id]].js
@@ -35,21 +35,22 @@ const ProductsManager = () => {
         const files = [...e.target.files]
         
         if(files.length === 0) 
-        return dispatch({type: 'NOTIFY', payload: {error: 'Files does not exist.'}})
+            return dispatch({type: 'NOTIFY', payload: {error: 'Files does not exist.'}})
 
         files.forEach(file => {
             if(file.size > 1024 * 1024)
-            return err = 'The largest img size is 1mb'
+                return err = 'The largest img size is 1mb'
 
-            if(files.type !== 'image/jpeg' && file.type !== 'image/png')
-            return err = 'Image format is incorrect'
-
+            if(file.type !== 'image/jpg' && file.type !== 'image/png')
+                return err = 'Image format is incorrect'
+            
             num += 1
             if(num <= 5) newImages.push(file)
-            return newImages
+                return newImages
+                
         })
 
-        if(err) dispatch({type: 'NOTIFY', payload: {error: err.msg}})
+        if(err) dispatch({type: 'NOTIFY', payload: {error: err}})
 
         const imgCount = images.length
         if(imgCount + newImages.length > 5)
