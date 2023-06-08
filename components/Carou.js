bb27e0ff95d61fe0b94c30fc286669a00d362849
Dung Nguyen <dungnov21@gmail.com>
2023-06-08 16:44:54 +0700

    new

--- components/Carou.js
+++ components/Carou.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Carou = () => {
     return (
-        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleIndicators" className="carousel slide " data-bs-ride="carousel">
             <div className="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ const Home = (props) => {
           <title>Home Page</title>
       </Head>
 
-      <Carou/>
+      {/* <Carou/> */}
 
       <Filter state={state}/>
 
